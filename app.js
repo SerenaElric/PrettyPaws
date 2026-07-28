b48ce09938bcf2090a910f71b8997acdf937e079
SerenaElric <agraham43@student.alamo.edu>
2026-07-27 23:37:51 -0500

    more

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log(hello)
+const lightbox = document.querySelector(".gallery-container .light-box");
+const lightboxImage = document.querySelector(".gallery-container .light-box img");
+const nextBtn = document.querySelector(".gallery-container .next-btn");
+const previousBtn = document.querySelector(".gallery-container .previous-btn");
+const closeBtn = document.querySelector(".gallery-container .close-btn");
+
+let currentImage = "";
+
+
+const showImage = (data) => {
+        currentImage = data;
+    lightbox.classList.add("active");
+    let image = data.querySelector("img");
+
+    lightboxImage.src = image.src;
+};
+
+closeBtn.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+});
+
+nextBtn.addEventListener("click", () => {
+    if (currentImage.nextElementSibling) {
+        currentImage = currentImage.nextElementSibling;
+        showImage(currentImage);
+    }
+});
+
+previousBtn.addEventListener("click", () => {
+    if (currentImage.previousElementSibling) {
+        currentImage = currentImage.previousElementSibling;
+        showImage(currentImage);
+    }
+});
